6ba846993260efa1d6d313f2c08056bc663bed30
Kseniia Golub <golubkseniia47@gmail.com>
2022-01-14 21:25:17 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let res = 0;
+    n = Math.abs(n);
+    while (n > 0.9) {
+        res = res * 10 + n % 10;
+        n = Math.trunc(n / 10);
+    }
+    return res;
 }
